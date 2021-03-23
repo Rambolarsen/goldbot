d9e1dc5c024e11a34f78c44285552ca19d1125cc
Rambolarsen <rambolarsen@gmail.com>
2021-03-23 09:44:22 +0100

    Add aliaces to price

--- src/bot.py
+++ src/bot.py
@@ -11,7 +11,7 @@ BOT = commands.Bot(command_prefix='!')
 
 FIREMAWGOLDPRICES = Scraper('https://www.g2g.com/wow-classic-eu/gold-27815-27817?server=33751&faction=33739&sorting=lowest_price')
 
-@BOT.group(name='price', invoke_without_command=True, aliases=['ony'])
+@BOT.group(name='price', invoke_without_command=True, aliases=['p'])
 async def price_cmd(ctx):
     for price in FIREMAWGOLDPRICES.get_gold_prices():
         await ctx.send(price)   
